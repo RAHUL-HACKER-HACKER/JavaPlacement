9eac976a8085528b87e6a8a83e36c1bd1b40d0f3
RAHUL-HACKER-HACKER <rahulk841501@gmail.com>
2023-12-02 15:03:39 +0530

    changes 3

--- AMAZON CODES/Replacing 0's with 5's/abc.java
+++ AMAZON CODES/Replacing 0's with 5's/abc.java
@@ -2,20 +2,5 @@ import java.util.*;
 class abc
 {
     //java code
-    public static void main(String[] args) {
-        int n;
-        Scanner sc=new Scanner(System.in);
-        n=sc.nextInt();
-        int c=0,s=0;
-        for(int i=n;i>0;i/=10)
-        {
-            if(i%10==0)
-            {
-                s+=5*(int)(Math.pow(10,c));
-            }
-            c++;
-        }
-        System.out.println(s+n);
-        sc.close();
-    }
+    
 }
