6c652f922c6919b5debdda3edaa7abc6d3ade4ee
RAHUL-HACKER-HACKER <rahulk841501@gmail.com>
2023-12-05 15:05:42 +0530

    modified

--- ARRAYS/3-SUM/abc.java
+++ ARRAYS/3-SUM/abc.java
@@ -3,10 +3,7 @@ class abc
 {
     //sum
     public static void main(String[] args) {
-        int n;
-        Scanner sc=new Scanner(System.in);
-        n=sc.nextInt();
-        int nums[]=new int[n];
+       
         for(int i=0;i<n;i++)
         {
             nums[i]=sc.nextInt();
